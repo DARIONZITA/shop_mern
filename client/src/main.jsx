3a8a0d6fe4c7a37c93802bc0810512ea4ab418ec
Yusof Paciente <yusofpaciente29@gmail.com>
2023-04-27 05:18:02 +0800

    updated router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <Toaster position="top-center" reverseOrder={false} />
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
